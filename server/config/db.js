7e840fbd45779862910b99c37e78d4790319fbbe
Keerthana Lahari <228905911+keerthana911-netizen@users.noreply.github.com>
2026-09-14 18:34:46 +0530

    Remove debug log for MONGO_URI
    Removed debug line that logs MONGO_URI.

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log("MONGO_URI:", process.env.MONGO_URI); // debug line
-
+    
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected 🚀");
